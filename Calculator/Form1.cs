1820a8c23e5e9a487d4ea5c0a1758427df6d8830
LeDoHuyenTram <tramle63463@gmail.com>
2021-11-04 00:56:19 +0700

    Da xu ly conflit

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -20,7 +20,7 @@ namespace Calculator
         private void btCong_Click(object sender, EventArgs e)
         {
             int a, b, ketQua;
-            if (txtA.Text != String.Empty)
+            if (txtA.Text != String.Empty || txtB.Text !=String.Empty)
             {
                 a = int.Parse(txtA.Text);
                 b = int.Parse(txtB.Text);
